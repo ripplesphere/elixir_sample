0d5f710b93903c4396373f57d96165dd9b1b5bde
George Mills <george.r.mills@gmail.com>
2017-02-25 21:25:17 -0500

    All alerts loaded into the repo. Had to update the schema from last commit.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -12,19 +12,21 @@
 
 alerts = File.read!("/home/george/mbta/waze_alerts.json") |> Poison.decode!() # this should probably be exsoda but the module failed in multiple locations when tests were run
 a_data = alerts["data"]
-my_list = hd(a_data)
 
 # Definitely could use some error handling in throughout here.
-{time_end, _} = List.pop_at(my_list, 8)
-{_, timee, _} = DateTime.from_iso8601(time_end <> "-05:00")
-time_end_f = Ecto.Time.cast!(timee)
-{location_st, _} = List.pop_at(my_list, 9)
-{location_city, _} = List.pop_at(my_list, 10)
-{locx, _} = List.pop_at(my_list, 13)
-{locy, _} = List.pop_at(my_list, 14)
-{hazard, _} = List.pop_at(my_list, 15)
-{hazard_type, _} = List.pop_at(my_list, 18)
-{time_start, _} = List.pop_at(my_list, 22)
-{_, times, _} = DateTime.from_iso8601(time_start <> "-05:00")
-time_start_f = Ecto.Time.cast!(times)
-ElixirSample.Repo.insert!(%ElixirSample.Alert{time_end: time_end_f, location_st: location_st, location_city: location_city, locx: locx, locy: locy, hazard: hazard, hazard_type: hazard_type, time_start: time_start_f})
+Enum.map(a_data, fn my_list -> 
+      {time_end, _} = List.pop_at(my_list, 8)
+      {_, timee, _} = DateTime.from_iso8601(time_end <> "-05:00")
+      time_end_f = Ecto.DateTime.cast!(timee)
+      {location_st, _} = List.pop_at(my_list, 9)
+      {location_city, _} = List.pop_at(my_list, 10)
+      {locx, _} = List.pop_at(my_list, 13)
+      {locy, _} = List.pop_at(my_list, 14)
+      {hazard, _} = List.pop_at(my_list, 15)
+      {hazard_type, _} = List.pop_at(my_list, 18)
+      {time_start, _} = List.pop_at(my_list, 22)
+      {_, times, _} = DateTime.from_iso8601(time_start <> "-05:00")
+      time_start_f = Ecto.DateTime.cast!(times)
+      ElixirSample.Repo.insert!(%ElixirSample.Alert{time_end: time_end_f, location_st: location_st, location_city: location_city, locx: locx, locy: locy, hazard: hazard, hazard_type: hazard_type, time_start: time_start_f})
+      end
+   )
--- test/controllers/alert_controller_test.exs
+++ test/controllers/alert_controller_test.exs
@@ -2,7 +2,7 @@ defmodule ElixirSample.AlertControllerTest do
   use ElixirSample.ConnCase
 
   alias ElixirSample.Alert
-  @valid_attrs %{hazard: "some content", hazard_type: "some content", location_city: "some content", location_st: "some content", locx: "some content", locy: "some content", time_end: %{hour: 14, min: 0, sec: 0}, time_start: %{hour: 14, min: 0, sec: 0}}
+  @valid_attrs %{hazard: "some content", hazard_type: "some content", location_city: "some content", location_st: "some content", locx: "some content", locy: "some content", time_end: %{day: 17, hour: 14, min: 0, month: 4, sec: 0, year: 2010}, time_start: %{day: 17, hour: 14, min: 0, month: 4, sec: 0, year: 2010}}
   @invalid_attrs %{}
 
   test "lists all entries on index", %{conn: conn} do
