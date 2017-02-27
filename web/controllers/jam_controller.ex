a215a839d37eaf06c59dc9bb536aa74b475b8e46
George Mills <george.r.mills@gmail.com>
2017-02-26 19:14:54 -0500

    Added hazard category and count for each category page.

--- web/controllers/jam_controller.ex
+++ web/controllers/jam_controller.ex
@@ -8,6 +8,24 @@ defmodule ElixirSample.JamController do
                select: {count(a.id)},
                where: a.hazard_type == "JAM")
       tot_num = Number.Delimit.number_to_delimited(elem(hd(Repo.all(query)), 0), precision: 0)
-      render(conn, "index.html", tot_num: tot_num)
+
+      query = from(a in Alert,
+                distinct: true,
+                select: a.hazard,
+                where: a.hazard_type == "JAM")
+      [ _ | hazard_classes ] = Repo.all(query)
+
+      hazard_count = Enum.map(hazard_classes, fn hazard ->
+         query = from(a in Alert, 
+                     select: {count(a.id)}, 
+                     where: a.hazard_type == "JAM" and a.hazard == ^hazard)
+         num_reported = Number.Delimit.number_to_delimited(elem(hd(Repo.all(query)), 0), precision: 0)
+         end
+      )
+
+      hazard_row = Enum.zip(hazard_classes, hazard_count)
+
+      render(conn, "index.html", tot_num: tot_num, hazard_row: hazard_row)
+
    end
 end
--- web/controllers/weatherhazard_controller.ex
+++ web/controllers/weatherhazard_controller.ex
@@ -8,6 +8,23 @@ defmodule ElixirSample.WeatherhazardController do
                select: {count(a.id)},
                where: a.hazard_type == "WEATHERHAZARD")
       tot_num = Number.Delimit.number_to_delimited(elem(hd(Repo.all(query)), 0), precision: 0)
-      render(conn, "index.html", tot_num: tot_num)
+
+      query = from(a in Alert,
+                distinct: true,
+                select: a.hazard,
+                where: a.hazard_type == "WEATHERHAZARD")
+      [ _ | hazard_classes ] = Repo.all(query)
+
+      hazard_count = Enum.map(hazard_classes, fn hazard ->
+         query = from(a in Alert, 
+                     select: {count(a.id)}, 
+                     where: a.hazard_type == "WEATHERHAZARD" and a.hazard == ^hazard)
+         num_reported = Number.Delimit.number_to_delimited(elem(hd(Repo.all(query)), 0), precision: 0)
+         end
+      )
+
+      hazard_row = Enum.zip(hazard_classes, hazard_count)
+
+      render(conn, "index.html", tot_num: tot_num, hazard_row: hazard_row)
    end
 end
